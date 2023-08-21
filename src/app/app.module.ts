b7441717f8bf673c749a866041f7570c66a9828d
Avelino <cleverson.avelino@dell.com>
2023-08-21 20:44:28 -0300

    adding forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatRadioModule} from '@angular/material/radio';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatRadioModule } from '@angular/material/radio';
 import { LayoutComponent } from './layout/layout.component';
 import { UsuarioComponent } from './usuario/usuario.component';
 import { ProdutoComponent } from './produto/produto.component';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { ProdutoComponent } from './produto/produto.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatRadioModule
+    MatRadioModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-usuario',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./usuario.component.css']
 })
 export class UsuarioComponent {
+
+  login = new FormControl('', 
+    [Validators.required, Validators.minLength(5)]);
+  nome = new FormControl('');
+  email = new FormControl('');
+
+  salvar() {
+    console.log(this.login.value);
+    console.log(this.nome.value);
+    console.log(this.email.value);
+  }
 
 }
